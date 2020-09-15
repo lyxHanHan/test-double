d9b303c9ddd432e5222534c0d58432aa912e95ff
liyixuan <13069579956@163.com>
2020-09-15 22:05:25 +0800

    modify the stub

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -8,17 +8,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GradeServiceTest {
 
-    GradeService gradeService;
+    private GradeService gradeService;
     GradeSystemStub gradeSystemStub;
-
     @BeforeEach
     public void setUp(){
         GradeSystemStub gradSystemSubs = new GradeSystemStub();
-        GradeService gradeService = new GradeService(gradSystemSubs);
+        gradeService = new GradeService(gradSystemSubs);
     }
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-
         double result = gradeService.calculateAverageGrades(1L);
         assertEquals(90.0, result);
     }
